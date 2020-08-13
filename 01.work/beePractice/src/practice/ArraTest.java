69bc08449bbc208a33313b761d84777bceb539a3
bee <22bmad@gmail.com>
2020-08-14 00:42:49 +0900

    array test2

--- 01.work/beePractice/src/practice/ArraTest.java
+++ 01.work/beePractice/src/practice/ArraTest.java
@@ -1,32 +1,33 @@
 package practice;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class ArraTest {
 
 	public static void main(String[] args){
+		
 		Scanner scan = new Scanner(System.in);
 		
-		int count = 0;
-		int max = 0;
-		int min = 0;
-		
-		count = scan.nextInt();
-		
-		int[] iArray = new int[count];
-		
-		for(int i = 0; i < count ; i++){
-			iArray[i] = scan.nextInt();
-
-		}
-
-		Arrays.sort(iArray);
-
-		min = iArray[0];
-		max = iArray[count-1];
-
-		System.out.println(min + " " + max);
-		
+		int maxNum = 0; //가장 큰 수
+		int maxNo = 0;	//가장 큰 수의 번호
+		int cnt = 1;
+		int[] iArray = new int[9];
+		Map<Integer, Integer> hm = new HashMap<>();
+			
+			
+			for(int i = 0; i < 9; i++){
+				iArray[i] = scan.nextInt();
+				hm.put(iArray[i], cnt++);
+			}
+			
+			Arrays.sort(iArray);
+			maxNum = iArray[8];
+			
+			maxNo = hm.get(maxNum);
+			System.out.println(maxNum);
+			System.out.println(maxNo);
 	}
 }
