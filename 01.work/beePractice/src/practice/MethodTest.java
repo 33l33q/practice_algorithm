c1598e903f2eef50f1f4f92d5efefd1e1fbc47b3
bee <22bmad@gmail.com>
2020-08-26 00:50:00 +0900

    array test3 1065_2번

--- 01.work/beePractice/src/practice/MethodTest.java
+++ 01.work/beePractice/src/practice/MethodTest.java
@@ -2,61 +2,62 @@ package practice;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-
+//1065
 public class MethodTest {
 
-	public void mTest1(){
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in) );
+	public static void main(String args[]){
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		try{
-			int iVal = 0, cnt = 1;
+			int iVal = 0, cnt = 0, n = 0;
 			String sVal = null;
 			int[] iArray = null;
-			int[] dArray= new int[2];
-			
+			int[] dArray = null;
 
-			if(sVal.length() == 1 || sVal.length() ==  2 ){
-				for(int i = 0 ; i < Integer.parseInt(sVal); i++){
-					cnt++;
-				}
-			}else{
+			sVal = br.readLine();
+			
+			if(sVal.length() > 3) sVal = "999";
+			
+			if(sVal.length() == 1 || sVal.length() == 2) cnt = Integer.parseInt(sVal);
+			
+			else if(sVal.length() == 3 ){
 				
-			}
-			
-			
-			for(int i = 0 ; i < sVal.length() ; i++){
-				iArray = new int[sVal.length()];
-				iArray[i] = sVal.charAt(i) - 48;
+				for(int i = 100 ; i <= Integer.parseInt(sVal) ; i++ ){
+					
+					iArray = new int[sVal.length()];
+					dArray = new int[sVal.length() - 1];
+					String iTs = Integer.toString(i);
 	
-			}
-			
-			
-			for(int j = 1 ; j < sVal.length() ; j++){
-				dArray = new int[sVal.length() - 1] ; 
-				dArray[j-1] = iArray[j] - iArray[j-1];
+					for(int j = 0 ; j < sVal.length() ; j++){
+						iArray[j] = (iTs.charAt(j) - 48 );
+					}
+					
+					dArray[0] = iArray[1] -  iArray[0];
+					dArray[1] = iArray[2] -  iArray[1];
+					
+					
+					if(dArray[0] == dArray[1]){ 
+						
+						cnt++;
+					}
+
+				}
 				
-				if(sVal.length() == 1) cnt++;
-				else if(sVal.length() == 3) if(dArray[0] == dArray[1]) cnt++;
-			
-			
+				cnt = cnt + 99 ;
 			}
+				
+				
+			
+			System.out.println(cnt);
+
 		
 		}catch(Exception e){
 			
 		}
-	}
-
-	
-	public static void main(String[] args) {
-
-		
-		
-		MethodTest mt = new MethodTest();
-		
 		
 	}//end of main
 
 }//end of class
+
 
